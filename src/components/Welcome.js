a4ff01cd57357c6f9795ab94853befee8af64101
pyang26 <dhyang10004@gmail.com>
2024-10-14 20:38:48 -0400

    Edit welcome page and add fade-in function when opening chat function

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,7 +14,7 @@ const Welcome = () => {
     <main className="welcome">
       <h2>Welcome to React Chat.</h2>
       <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
-      <p>Sign in with Google to chat with with your fellow React Developers.</p>
+      <p>Sign in with Google to access our AI service.</p>
       <button className="sign-in">
         <img
           onClick={googleSignIn}
